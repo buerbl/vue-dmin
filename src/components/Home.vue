cb08a7b8ff763240566a13f4ac786741a47302d7
buer <18319748706@163.com>
2020-01-31 12:18:23 +0800

    :star:整体布局

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,15 +1,89 @@
 <template>
-    <div>
-        home组件
-    </div>
+  <el-container class="home">
+    <!-- 头部区域 -->
+    <el-header>
+      <div>
+        <span>后台管理系统</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <!-- 页面主体区域 -->
+    <el-container>
+      <!-- 侧边栏 -->
+      <el-aside width="200px">
+        <el-menu
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="#ffd04b"
+        >
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>导航一</span>
+            </template>
+            <el-menu-item index="1-1">选项1</el-menu-item>
+            <el-menu-item index="1-2">选项2</el-menu-item>
+          </el-submenu>
+          <el-submenu index="2">
+            <template slot="title">
+              <i class="el-icon-menu"></i>
+              <span>导航二</span>
+            </template>
+            <el-menu-item index="2-1">选项1</el-menu-item>
+            <el-menu-item index="2-2">选项2</el-menu-item>
+          </el-submenu>
+          <el-submenu index="3">
+            <template slot="title">
+              <i class="el-icon-document"></i>
+              <span>导航三</span>
+            </template>
+            <el-menu-item index="3-1">选项1</el-menu-item>
+            <el-menu-item index="3-2">选项2</el-menu-item>
+          </el-submenu>
+          <el-submenu index="4">
+            <template slot="title">
+              <i class="el-icon-setting"></i>
+              <span>导航四</span>
+            </template>
+            <el-menu-item index="4-1">选项1</el-menu-item>
+            <el-menu-item index="4-2">选项2</el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <!-- 右侧内容主体 -->
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
-    
-}
+  methods: {
+    logout() {}
+  }
+};
 </script>
 
 <style lang="less" scoped>
-
+.el-header {
+  background-color: #373c41;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: #fff;
+  font-size: 25px;
+  > div {
+    display: flex;
+    align-items: center;
+  }
+}
+.el-aside {
+  background-color: #323744;
+}
+.el-main {
+  background-color: #eaedf2;
+}
+.home {
+  height: 100%;
+}
 </style>
